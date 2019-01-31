69dd1ebe7b60749ebc63f0fee3552a1deec72907
elZeff <mist.zeffyrre@gmail.com>
2019-01-31 15:17:17 +0100

    tir

---
+++ Assets/Sript/panpan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panpan : MonoBehaviour {
+    public GameObject Projectil;
+    [SerializeField] private Transform CanonTip;
+    public int force = 1; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject Boule = Instantiate(Projectil, CanonTip.position, CanonTip.rotation) as GameObject;
+
+            }
+        }
+    }
