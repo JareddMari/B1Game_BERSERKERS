992eef6b9ae1c3d1a7d22e7d3787052b49eab4c3
Nercros <miniblond33@gmail.com>
2019-02-05 15:17:57 +0100

    Trajectoire du canon

---
+++ Assets/Sript/DestructionVfxTrajectoire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionVfxTrajectoire : MonoBehaviour {
+
+    public GameObject VfxTrajectoireTir;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	/*public void ParentCanon(GameObject Canon)
+    {
+        VfxTrajectoireTir.transform.parent = Canon.transform;
+        Debug.Log("Player's Parent: " + VfxTrajectoireTir.transform.parent.name);
+    }
+    */
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+*/}
---
+++ Assets/Sript/SetParent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParent : MonoBehaviour {
+
+    public GameObject Canon;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.parent = Canon.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
