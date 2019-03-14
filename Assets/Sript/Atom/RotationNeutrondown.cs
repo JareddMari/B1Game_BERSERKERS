31e189892896296ecca63d642c063c1b21533a9f
elZeff <mist.zeffyrre@gmail.com>
2019-03-14 14:52:17 +0100

    atom

---
+++ Assets/Sript/Atom/RotationNeutrondown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationNeutrondown : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 50f;
+    [HideInInspector]
+    public bool isPaused = false;
+    public KeyCode pauseKey;
+
+    public enum StartState { Rotate, Invert, Paused }
+    public StartState rotateState;
+    // Use this for initialization
+    void Start()
+    {
+        switch (rotateState)
+        {
+            case StartState.Invert:
+                rotationSpeed = -rotationSpeed;
+                break;
+            case StartState.Paused:
+                isPaused = true;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            ToggleRotationPause();
+        }
+        if (!isPaused)
+        {
+            transform.Rotate(Vector3.left, Time.deltaTime * rotationSpeed);
+        }
+    }
+    public void ToggleRotationPause()
+    {
+        isPaused = !isPaused;
+    }
+
+}
